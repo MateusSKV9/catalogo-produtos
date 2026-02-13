67f8779d148aba836f9ad0ae8b268ccfb50bf8e9
Mateus Santos <mateussilvanzg@gmail.com>
2026-02-13 19:50:31 -0300

    chore: improve accessibility

--- src/features/products/components/ProductTable/ProductTable.jsx
+++ src/features/products/components/ProductTable/ProductTable.jsx
@@ -4,7 +4,7 @@ import { ProductRow } from "../ProductRow/ProductRow";
 export function ProductTable({ products, onDelete }) {
 	return (
 		<ul className={styles.table}>
-			<div className={styles.header}>
+			<li className={styles.header}>
 				<div className={styles.row}>
 					<div className={styles.first_coll}>
 						<span className={styles.th}>Produto</span>
@@ -13,13 +13,15 @@ export function ProductTable({ products, onDelete }) {
 					</div>
 					<span className={`${styles.actions} ${styles.th}`}>Ações</span>
 				</div>
-			</div>
+			</li>
 
-			<ul className={styles.body}>
-				{products.map((product) => (
-					<ProductRow key={product.id} product={product} onDelete={onDelete} />
-				))}
-			</ul>
+			<li>
+				<ul className={styles.body}>
+					{products.map((product) => (
+						<ProductRow key={product.id} product={product} onDelete={onDelete} />
+					))}
+				</ul>
+			</li>
 		</ul>
 	);
 }
